dd254ec8cba0b062cf141c1bb36e47efb44693c3
SADMAN MUTTAKI <24077386@siswa.um.edu.my>
2025-03-28 21:32:35 +0800

    Add files via upload

---
+++ Tutorial 1/T01Q03.java
@@ -0,0 +1,21 @@
+package com.mycompany.t01q03;
+
+import java.util.Scanner;
+
+public class T01Q03 {
+
+    public static void main(String[] args){
+        
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("Enter your obtained mark: ");
+    int mark = input.nextInt();
+    
+    if (mark >= 40) {
+    System.out.println("Congratulations! You are passed in the examination");
+    }
+    else {
+    System.out.println("You are failed in the examination.");
+    }
+   }
+}
---
+++ Tutorial 1/T01Q12.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t01q12;
+
+/**
+ *
+ * @author sadmanmuttaki
+ */
+public class T01Q12 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
