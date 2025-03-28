9ed80a5229c152457a06b6b10e1e5ea72d115f47
SADMAN MUTTAKI <24077386@siswa.um.edu.my>
2025-03-28 21:33:48 +0800

    Add files via upload

---
+++ LAB 1/L1Q3.java
@@ -0,0 +1,15 @@
+package com.mycompany.l1q3;
+
+public class L1Q3 {
+    public static void main(String[] args) {
+       System.out.println("********* \t     ***      \t      *     \t      *       ");
+       System.out.println("*       * \t   *     *    \t     ***    \t     * *      ");
+       System.out.println("*       * \t  *       *   \t    *****   \t    *   *     ");
+       System.out.println("*       * \t  *       *   \t      *     \t   *     *    ");
+       System.out.println("*       * \t  *       *   \t      *     \t  *       *   ");
+       System.out.println("*       * \t  *       *   \t      *     \t   *     *    ");
+       System.out.println("*       * \t  *       *   \t      *     \t    *   *     ");
+       System.out.println("*       * \t   *     *    \t      *     \t     * *      ");
+       System.out.println("********* \t     ***      \t      *     \t      *       ");
+    }
+}
---
+++ LAB 1/L1q2MyProfile.java
@@ -0,0 +1,21 @@
+package com.mycompany.l1q2.myprofile;
+
+public class L1q2MyProfile {
+
+public static void main (String[] args){
+     String fullName = "Sadman Muttaki";
+     String dateOfBirth = "29.12.2006";
+     String matricNumber = "24077386";
+     String address = "Pelangi Utama Condominium, Universiti Malaya";
+     String email = "24077386@siswa.um.edu.my";
+     String contactNumber = "+60103847922";
+             
+     System.out.println("Personal Profile: ");
+     System.out.println("Full Name: " + fullName);
+     System.out.println("Date of birth: " + dateOfBirth);
+     System.out.println("Matric Number: " + matricNumber);
+     System.out.println("Address: " + address);
+     System.out.println("Email: " + email);
+     System.out.println("Contact Number: " + contactNumber);
+ }
+}
